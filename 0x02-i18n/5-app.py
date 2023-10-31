57bc0f18cf7b7f764683795384f5f2d9e56fd566
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-10-31 22:18:49 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,30 +2,20 @@
 """
 Flask app
 """
-from flask import (Flask, render_template, request, g)
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
 from flask_babel import Babel
 
+
 users = {
-    1: {
-        "name": "Balou",
-        "locale": "fr",
-        "timezone": "Europe/Paris"
-    },
-    2: {
-        "name": "Beyonce",
-        "locale": "en",
-        "timezone": "US/Central"
-    },
-    3: {
-        "name": "Spock",
-        "locale": "kg",
-        "timezone": "Vulcan"
-    },
-    4: {
-        "name": "Teletubby",
-        "locale": None,
-        "timezone": "Europe/London"
-    },
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
 
