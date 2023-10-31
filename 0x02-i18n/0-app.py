51babd1eac713765332ccd81ff45f5e738feec69
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-10-31 15:56:40 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-# 0-app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
